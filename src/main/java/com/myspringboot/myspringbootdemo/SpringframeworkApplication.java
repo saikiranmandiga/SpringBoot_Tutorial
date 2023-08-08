37c2e68c22bd293c940fcf86926cf856b4b70a43
Saikiran <saikiranmandiga@gmail.com>
2023-08-08 14:47:57 -0500

    Change in main class

--- src/main/java/com/myspringboot/myspringbootdemo/SpringframeworkApplication.java
+++ src/main/java/com/myspringboot/myspringbootdemo/SpringframeworkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyspringbootdemoApplication {
+public class SpringframeworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyspringbootdemoApplication.class, args);
+		SpringApplication.run(SpringframeworkApplication.class, args);
 	}
 
 }
